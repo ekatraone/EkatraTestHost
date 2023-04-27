851ba3a5e526d63a1019025d63b1955cb534c66d
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2023-04-27 22:04:32 +0530

    SOlved issue with main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
