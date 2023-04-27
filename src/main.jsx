e37d7abd740d69b83916b5d87855b67dbe5ae03a
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2023-04-27 22:11:58 +0530

    fixed issue with main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { render } from "react-dom";
+
 import App from "./App";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const root = document.getElementById("root");
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+render(
   <Auth0Provider
-    domain= {import.meta.env.VITE_AUTH0_DOMAIN}
+    domain={import.meta.env.VITE_AUTH0_DOMAIN}
     clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <App />
-  </Auth0Provider>
+  </Auth0Provider>,
+  root
 );
